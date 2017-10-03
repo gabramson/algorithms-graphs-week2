df9f64e830b6e6f8b0b8c949241552947a23d0bb
Gary Abramson <gary.abramson@gmail.com>
2017-10-02 20:50:39 -0500

    initial

---
+++ DykstraTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DykstraTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
