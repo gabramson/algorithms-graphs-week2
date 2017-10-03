4ba53f1f26adbd8082762501063e789775ad8479
Gary Abramson <gary.abramson@gmail.com>
2017-10-02 23:38:12 -0500

    Completed Heap

--- Dykstra/Heap.cs
+++ Dykstra/Heap.cs
@@ -9,32 +9,71 @@ namespace Dykstra
         private List<(int key, valueType value)> heapList;
         int heapSize = 0;
 
-        private int ParentIndex(int pos)
-        {
-            return (pos + 1) / 2 - 1;
-        }
-
         public MinHeap()
         {
             heapList = new List<(int key, valueType value)>();
         }
 
-        public void Add(int key, valueType value)
+        private int ParentIndexOf(int pos)
         {
-            int pos;
-            heapSize += 1;
-            heapList.Add((key, value));
-            for (pos = heapSize-1; pos>0 && heapList[ParentIndex(pos)].key > key; pos = ParentIndex(pos))
-            {
-                heapList[pos] = heapList[ParentIndex(pos)];
-            }
-            heapList[pos] = (key, value);
+            return (pos + 1) / 2 - 1;
         }
 
-        public (int, valueType) RemoveMin()
+        private void PercolateUpFromBottom()
+        {
+            int pos;
+            (int key, valueType) tmp = heapList[heapSize-1];
+            for (pos = heapSize - 1; pos > 0 && heapList[ParentIndexOf(pos)].key > tmp.key; pos = ParentIndexOf(pos))
+            {
+                heapList[pos] = heapList[ParentIndexOf(pos)];
+            }
+            heapList[pos] = tmp;
+        }
+
+        public void Add(int key, valueType value)
+        {
+            heapList.Add((key, value));
+            heapSize += 1;
+            PercolateUpFromBottom();
+        }
+
+        private int LeftChildIndexOf(int pos)
+        {
+            return (2 * (pos + 1) - 1);
+        }
+
+        private void PercolateDownFromTop()
+        {
+            int pos = 0;
+            (int key, valueType) tmp = heapList[pos];
+            for (int child = LeftChildIndexOf(pos); child <= heapSize-1; pos = child, child = LeftChildIndexOf(pos))
+            {
+                if ((child < heapSize-1) && (heapList[child].key > heapList[child+1].key)) {
+                    child += 1;
+                }
+
+                if (heapList[child].key < tmp.key)
+                {
+                    heapList[pos] = heapList[child];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            heapList[pos] = tmp;
+        }
+
+        public (int, valueType) ExtractMin()
         {
             (int key, valueType value) returnValue = heapList[0];
-            heapList.RemoveAt(0);
+            heapList[0] = heapList[heapSize - 1];
+            heapList.RemoveAt(heapSize - 1);
+            heapSize -= 1;
+            if (heapSize > 0)
+            {
+                PercolateDownFromTop();
+            }
             return (returnValue);
         }
     }
